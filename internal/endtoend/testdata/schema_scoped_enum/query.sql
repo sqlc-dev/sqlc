b201893e50dcac8df2e171646234af7c4c8a9079
Kyle Conroy <kyle@conroy.org>
2020-01-30 08:17:12 -0800

    internal/dinosql: Gandle schema-scoped enums

---
+++ internal/endtoend/testdata/schema_scoped_enum/query.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA foo;
+
+CREATE TYPE foo.type_user_role AS ENUM ('admin', 'user');
+
+CREATE TABLE foo.users (
+    role foo.type_user_role
+);
+
+SELECT * FROM foo.users WHERE role = $1;
