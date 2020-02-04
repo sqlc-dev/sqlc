19b56aacb2e87c3a4313c363a84643cd7d873dc5
Kyle Conroy <kyle@conroy.org>
2020-02-03 17:18:30 -0800

    internal/mysql: Port tests to endtoend package (#315)

---
+++ internal/endtoend/testdata/mysql_param/query.sql
@@ -0,0 +1,27 @@
+/* name: ListUsersByID :many */
+SELECT first_name, id, last_name FROM users WHERE id < ?;
+
+/* name: ListUserOrders :many */
+SELECT
+	users.id,
+	users.first_name,
+	orders.price
+FROM
+	orders
+LEFT JOIN users ON orders.user_id = users.id
+WHERE orders.price > :minPrice;
+
+/* name: GetUserByID :one */
+SELECT first_name, id, last_name FROM users WHERE id = :targetID;
+
+/* name: ListUsersByFamily :many */
+SELECT first_name, last_name FROM users WHERE age < :maxAge AND last_name = :inFamily;
+
+/* name: ListUsersWithLimit :many */
+SELECT first_name, last_name FROM users LIMIT ?;
+
+/* name: LimitSQLCArg :many */
+select first_name, id FROM users LIMIT sqlc.arg(UsersLimit);
+
+/* name: InsertNewUser :exec */
+INSERT INTO users (first_name, last_name) VALUES (?, sqlc.arg(user_last_name));
