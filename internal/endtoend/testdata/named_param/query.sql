f09eeb8ab55578ba743f87d5657665cf046a0b2d
Kyle Conroy <kyle@conroy.org>
2020-02-10 15:06:40 -0800

    internal/endtoend: Add update test for named params

--- internal/endtoend/testdata/named_param/query.sql
+++ internal/endtoend/testdata/named_param/query.sql
@@ -5,3 +5,12 @@ SELECT name FROM foo WHERE name = sqlc.arg(slug) AND sqlc.arg(filter)::bool;
 
 -- name: AtParams :many
 SELECT name FROM foo WHERE name = @slug AND @filter::bool;
+
+-- name: Update :one
+UPDATE foo
+SET
+  name = CASE WHEN @set_name::bool
+    THEN @name::text
+    ELSE name
+    END
+RETURNING *;
