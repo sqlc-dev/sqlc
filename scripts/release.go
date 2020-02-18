2471b6d890bdffe92d95dbb0ea0b7cd829bc45b4
Kyle Conroy <kyle@conroy.org>
2020-02-18 11:00:10 -0800

    Create release for tags (#346)
    * release: Push release to stable branch on tag
    
    * actions: Release new versions on tag creation

--- scripts/release.go
+++ scripts/release.go
@@ -13,7 +13,13 @@ func main() {
 	draft := flag.Bool("draft", false, "create a draft release")
 	flag.Parse()
 
+	arch := flag.Arg(0)
+	if arch == "" {
+		log.Fatalf("missing platform_arch argument")
+	}
+
 	sha := os.Getenv("GITHUB_SHA")
+	ref := os.Getenv("GITHUB_REF")
 	cmd := exec.Command("git", "show", "--no-patch", "--no-notes", "--pretty=%ci", sha)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
@@ -26,16 +32,19 @@ func main() {
 	date = strings.Replace(date, ":", "", -1)
 	version := fmt.Sprintf("v0.0.0-%s-%s", date, sha[:12])
 
-	x := "-X github.com/kyleconroy/sqlc/internal/cmd.version=" + version
-	log.Printf("Releasing %s on channel %s", flag.Arg(1), flag.Arg(0))
-
 	xname := "./equinox"
 	if _, err := os.Stat("./equinox"); os.IsNotExist(err) {
 		xname = "equinox"
 	}
 
+	channel := "devel"
+	if strings.HasPrefix(ref, "refs/tags/") {
+		channel = "stable"
+		version = strings.TrimPrefix(ref, "refs/tags/")
+	}
+
 	args := []string{"release",
-		"--channel", flag.Arg(0),
+		"--channel", channel,
 		"--version", version,
 	}
 
@@ -43,14 +52,16 @@ func main() {
 		args = append(args, "--draft")
 	}
 
+	x := "-X github.com/kyleconroy/sqlc/internal/cmd.version=" + version
 	args = append(args, []string{
-		"--platforms", flag.Arg(1),
+		"--platforms", flag.Arg(0),
 		"--app", "app_i4iCp1SuYfZ",
 		"--token", os.Getenv("EQUINOX_API_TOKEN"),
 		"--",
 		"-ldflags", x, "./cmd/sqlc",
 	}...)
 
+	log.Printf("Releasing %s on channel %s", flag.Arg(0), channel)
 	cmd = exec.Command(xname, args...)
 	cmd.Env = os.Environ()
 	out, err = cmd.CombinedOutput()
