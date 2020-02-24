4b756bb73dec68e595408624f0ce16e623501da1
Alex Besogonov <Alex.Besogonov@gmail.com>
2020-02-24 11:28:18 -0800

    Allow sqlc.arg('argname') form for named params (#351)
    * Allow sqlc.arg('argname') form for named params
    
    This pacifies static SQL analizers in IDEs.
    
    * Allow sqlc to be imported as a package for "go run github.com/kyleconroy/sqlc"
    
    Co-authored-by: Kyle Conroy <kyle@conroy.org>

--- internal/endtoend/testdata/named_param/query.sql
+++ internal/endtoend/testdata/named_param/query.sql
@@ -1,7 +1,7 @@
 CREATE TABLE foo (name text not null, bio text not null);
 
 -- name: FuncParams :many
-SELECT name FROM foo WHERE name = sqlc.arg(slug) AND sqlc.arg(filter)::bool;
+SELECT name FROM foo WHERE name = sqlc.arg('slug') AND sqlc.arg(filter)::bool;
 
 -- name: AtParams :many
 SELECT name FROM foo WHERE name = @slug AND @filter::bool;
