dcf1a6c920fd6f1891b378cb36aefe3a08ae1372
Kyle Conroy <kyle@conroy.org>
2020-05-29 11:22:57 -0700

    Support SELECT * in MySQL  (#529)
    * Add MySQL support for select star
    
    * Remove trailing semicolon

--- internal/compiler/go_type.go
+++ internal/compiler/go_type.go
@@ -33,6 +33,23 @@ func (r *Result) goInnerType(col *Column, settings config.CombinedSettings) stri
 		}
 	}
 
+	// TODO: Extend the engine interface to handle types
+	switch settings.Package.Engine {
+	case config.EngineMySQL, config.EngineXDolphin:
+		return r.mysqlType(col, settings)
+	case config.EnginePostgreSQL:
+		return r.postgresType(col, settings)
+	case config.EngineXLemon:
+		return r.postgresType(col, settings)
+	default:
+		return "interface{}"
+	}
+}
+
+func (r *Result) postgresType(col *Column, settings config.CombinedSettings) string {
+	columnType := col.DataType
+	notNull := col.NotNull || col.IsArray
+
 	switch columnType {
 	case "serial", "pg_catalog.serial4":
 		if notNull {
@@ -184,5 +201,67 @@ func (r *Result) goInnerType(col *Column, settings config.CombinedSettings) stri
 
 		log.Printf("unknown PostgreSQL type: %s\n", columnType)
 		return "interface{}"
+	}
+}
+
+func (r *Result) mysqlType(col *Column, settings config.CombinedSettings) string {
+	columnType := col.DataType
+	notNull := col.NotNull || col.IsArray
+
+	switch columnType {
+
+	case "varchar", "text", "char", "tinytext", "mediumtext", "longtext":
+		if notNull {
+			return "string"
+		}
+		return "sql.NullString"
+
+	case "int", "integer", "smallint", "mediumint", "year":
+		if notNull {
+			return "int32"
+		}
+		return "sql.NullInt32"
+
+	case "bigint":
+		if notNull {
+			return "int64"
+		}
+		return "sql.NullInt64"
+
+	case "blob", "binary", "varbinary", "tinyblob", "mediumblob", "longblob":
+		return "[]byte"
+
+	case "double", "double precision", "real":
+		if notNull {
+			return "float64"
+		}
+		return "sql.NullFloat64"
+
+	case "decimal", "dec", "fixed":
+		if notNull {
+			return "string"
+		}
+		return "sql.NullString"
+
+	case "enum":
+		// TODO: Proper Enum support
+		return "string"
+
+	case "date", "timestamp", "datetime", "time":
+		if notNull {
+			return "time.Time"
+		}
+		return "sql.NullTime"
+
+	case "boolean", "bool", "tinyint":
+		if notNull {
+			return "bool"
+		}
+		return "sql.NullBool"
+
+	default:
+		log.Printf("unknown MySQL type: %s\n", columnType)
+		return "interface{}"
+
 	}
 }
