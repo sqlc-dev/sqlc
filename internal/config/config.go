fce60afdd9806ba9827b3ca9bebcbe5c04d97d6c
Philipp Tanlak <philipp.tanlak@gmail.com>
2020-06-19 13:53:02 +0200

    Add option to return empty slices in :many query, to improve usability with json apis

--- internal/config/config.go
+++ internal/config/config.go
@@ -115,6 +115,7 @@ type SQLGo struct {
 	EmitJSONTags        bool              `json:"emit_json_tags" yaml:"emit_json_tags"`
 	EmitPreparedQueries bool              `json:"emit_prepared_queries" yaml:"emit_prepared_queries"`
 	EmitExactTableNames bool              `json:"emit_exact_table_names,omitempty" yaml:"emit_exact_table_names"`
+	EmitEmptySlices     bool              `json:"emit_empty_slices,omitempty" yaml:"emit_empty_slices"`
 	Package             string            `json:"package" yaml:"package"`
 	Out                 string            `json:"out" yaml:"out"`
 	Overrides           []Override        `json:"overrides,omitempty" yaml:"overrides"`
