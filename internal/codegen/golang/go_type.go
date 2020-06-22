15761a4635232b02b982d6a099ab5ec2f99a1277
Kyle Conroy <kyle@conroy.org>
2020-06-22 10:24:23 -0700

    golang: Add support for the money type (#561)

--- internal/codegen/golang/go_type.go
+++ internal/codegen/golang/go_type.go
@@ -93,7 +93,7 @@ func postgresType(r *compiler.Result, col *compiler.Column, settings config.Comb
 		}
 		return "sql.NullFloat64" // TODO: Change to sql.NullFloat32 after updating the go.mod file
 
-	case "pg_catalog.numeric":
+	case "pg_catalog.numeric", "money":
 		// Since the Go standard library does not have a decimal type, lib/pq
 		// returns numerics as strings.
 		//
