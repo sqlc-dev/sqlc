767ce378da965fcc1ea3899f6a7db90a97d94090
Kyle Conroy <kyle@conroy.org>
2020-06-26 10:22:20 -0700

    oops

--- internal/endtoend/testdata/sqlc_arg/postgresql/query.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE foo (name text not null, bio text not null);
-
--- name: FuncParams :many
-SELECT name FROM foo WHERE name = sqlc.arg('slug');
-
--- name: AtParams :many
-SELECT name FROM foo WHERE name = @name;
