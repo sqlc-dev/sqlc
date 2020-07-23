5ca69d75e647011ea0d347bd7d6e2a04f47b7e30
Jessica Bellon <10819486+JessTheBell@users.noreply.github.com>
2020-07-23 10:29:29 -0500

    mysql: allow functions to return null
    Issue #544

--- internal/mysql/parse.go
+++ internal/mysql/parse.go
@@ -451,7 +451,7 @@ func (pGen PackageGenerator) parseSelectAliasExpr(exprs sqlparser.SelectExprs, t
 					Name: returnVal,
 					Type: sqlparser.ColumnType{
 						Type:    funcType,
-						NotNull: true,
+						NotNull: false,
 					},
 				}
 				cols = append(cols, Column{colDfn, ""}) // func returns types don't originate from a table schema
