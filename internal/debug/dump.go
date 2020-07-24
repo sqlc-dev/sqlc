40cddd77060327504bff252caac63a4dc2dcda56
Kyle Conroy <kyle@conroy.org>
2020-07-24 15:35:33 -0700

    dolphin: Implement parameter refs

--- internal/debug/dump.go
+++ internal/debug/dump.go
@@ -1,8 +1,14 @@
 package debug
 
 import (
+	"os"
+
 	"github.com/davecgh/go-spew/spew"
 )
+
+func Active() bool {
+	return os.Getenv("SQLCDEBUG") != ""
+}
 
 func Dump(n interface{}) {
 	spew.Dump(n)
