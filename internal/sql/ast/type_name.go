7726953cb4bf3e95d8c05ed51db0b4ad61b6891c
Kyle Conroy <kyle@conroy.org>
2020-08-24 14:29:20 -0700

    No more PG types

--- internal/sql/ast/type_name.go
+++ internal/sql/ast/type_name.go
@@ -4,8 +4,18 @@ type TypeName struct {
 	Catalog string
 	Schema  string
 	Name    string
+
+	// From pg.TypeName
+	Names       *List
+	TypeOid     Oid
+	Setof       bool
+	PctType     bool
+	Typmods     *List
+	Typemod     int32
+	ArrayBounds *List
+	Location    int
 }
 
 func (n *TypeName) Pos() int {
-	return 0
+	return n.Location
 }
