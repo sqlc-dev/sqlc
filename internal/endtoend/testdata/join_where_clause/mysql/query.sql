b4a121e4bb1fff224f2abcea98c5176f0d1ccd4b
Kyle Conroy <kyle@conroy.org>
2020-08-27 17:52:01 -0700

    endtoend: Add MySQL join where clause test

---
+++ internal/endtoend/testdata/join_where_clause/mysql/query.sql
@@ -0,0 +1,8 @@
+CREATE TABLE foo (barid serial not null);
+CREATE TABLE bar (id serial not null, owner text not null);
+
+-- name: JoinWhereClause :many
+SELECT foo.*
+FROM foo
+JOIN bar ON bar.id = barid
+WHERE owner = ?;
