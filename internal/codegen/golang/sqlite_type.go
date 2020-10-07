3b484a60b28314dfb13c3c597d2eb36cf59ab189
Miha Vrhovnik <miha.vrhovnik@result.si>
2020-10-07 13:31:03 +0200

    Add option to use pointers instead of sql.Null* types

--- internal/codegen/golang/sqlite_type.go
+++ internal/codegen/golang/sqlite_type.go
@@ -11,12 +11,16 @@ import (
 func sqliteType(r *compiler.Result, col *compiler.Column, settings config.CombinedSettings) string {
 	dt := col.DataType
 	notNull := col.NotNull || col.IsArray
+	pointer := ""
+	if !notNull && settings.Go.UsePointers {
+		pointer = "*"
+	}
 
 	switch dt {
 
 	case "integer":
-		if notNull {
-			return "int32"
+		if notNull || pointer != "" {
+			return pointer + "int32"
 		}
 		return "sql.NullInt32"
 
@@ -28,8 +32,8 @@ func sqliteType(r *compiler.Result, col *compiler.Column, settings config.Combin
 	switch {
 
 	case strings.HasPrefix(dt, "varchar"):
-		if notNull {
-			return "string"
+		if notNull || pointer != "" {
+			return pointer + "string"
 		}
 		return "sql.NullString"
 
