159603831f1d1e8dced0f47f63f9ec3b509a1dc0
Kyle Conroy <kyle@conroy.org>
2020-11-19 08:45:51 -0800

    sql/catalog: Add support for variadic functions

---
+++ internal/endtoend/testdata/json_build/query.sql
@@ -0,0 +1,13 @@
+-- name: SelectJSONBuildObject :one
+SELECT
+  json_build_object('foo'),
+  json_build_object('foo', 1),
+  json_build_object('foo', 1, 2),
+  json_build_object('foo', 1, 2, 'bar');
+
+-- name: SelectJSONBuildArray :one
+SELECT 
+  json_build_array(1),
+  json_build_array(1, 2),
+  json_build_array(1, 2, 'foo'),
+  json_build_array(1, 2, 'foo', 4);
