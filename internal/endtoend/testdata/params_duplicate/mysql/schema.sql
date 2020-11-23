3d23cd9c716ef111608109bfc5f7a078db347867
Kyle Conroy <kyle@conroy.org>
2020-11-22 18:11:56 -0800

    Generate valid code for MySQL named parameters (#805)

--- internal/endtoend/testdata/params_duplicate/mysql/schema.sql
+++ internal/endtoend/testdata/params_duplicate/mysql/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
     id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name varchar(255) NOT NULL,
+    first_name varchar(255),
     last_name varchar(255)
 ) ENGINE=InnoDB;
