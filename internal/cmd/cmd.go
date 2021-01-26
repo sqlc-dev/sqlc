051f62dc4d8eb51c01ea1d9d475126c18f2d3481
Kyle Conroy <kyle@conroy.org>
2021-01-25 23:01:56 -0800

    Fix description

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -18,7 +18,7 @@ import (
 // Do runs the command logic.
 func Do(args []string, stdin io.Reader, stdout io.Writer, stderr io.Writer) int {
 	rootCmd := &cobra.Command{Use: "sqlc", SilenceUsage: true}
-	rootCmd.PersistentFlags().StringP("file", "f", "", "specify an alternate config file (default: sqlc.yml)")
+	rootCmd.PersistentFlags().StringP("file", "f", "", "specify an alternate config file (default: sqlc.yaml)")
 
 	rootCmd.AddCommand(checkCmd)
 	rootCmd.AddCommand(genCmd)
