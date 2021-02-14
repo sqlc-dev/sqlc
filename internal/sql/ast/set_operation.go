2048e0fdb9046cb9ed3854ce87d5af47e3040594
aitva <aitva@pm.me>
2021-02-14 19:43:31 +0100

    sql/ast: add enum values for SetOperation
    This commit adds enum values for SetOperation. The values are extracted from
    the parser sources in pg_query_go:
    
    typedef enum SetOperation
    {
        SETOP_NONE = 0,
        SETOP_UNION,
        SETOP_INTERSECT,
        SETOP_EXCEPT
    } SetOperation;

--- internal/sql/ast/set_operation.go
+++ internal/sql/ast/set_operation.go
@@ -1,7 +1,31 @@
 package ast
 
+import "strconv"
+
+const (
+	None SetOperation = iota
+	Union
+	Intersect
+	Except
+)
+
 type SetOperation uint
 
 func (n *SetOperation) Pos() int {
 	return 0
+}
+
+func (n SetOperation) String() string {
+	switch n {
+	case None:
+		return "None"
+	case Union:
+		return "Union"
+	case Intersect:
+		return "Intersect"
+	case Except:
+		return "Except"
+	default:
+		return "Unknown(" + strconv.FormatUint(uint64(n), 10) + ")"
+	}
 }
