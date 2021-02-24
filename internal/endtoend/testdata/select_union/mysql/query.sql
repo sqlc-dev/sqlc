b752ff0404823afd94404be97041939aeb69635d
aitva <aitva@pm.me>
2021-02-24 16:39:13 +0100

    endtoend: add tests for INTERSECT and EXCEPT

--- internal/endtoend/testdata/select_union/mysql/query.sql
+++ internal/endtoend/testdata/select_union/mysql/query.sql
@@ -4,3 +4,13 @@ CREATE TABLE foo (a text, b text);
 SELECT * FROM foo
 UNION
 SELECT * FROM foo;
+
+-- name: SelectExcept :many
+SELECT * FROM foo
+EXCEPT
+SELECT * FROM foo;
+
+-- name: SelectIntersect :many
+SELECT * FROM foo
+INTERSECT
+SELECT * FROM foo;
