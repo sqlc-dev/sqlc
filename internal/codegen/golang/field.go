77738fd2f9e48bb81217225976d3b5f7b52c83ae
Tommy Chu <tommychu2256@gmail.com>
2021-03-08 21:27:14 +0100

    feat(json-tags): Add case style config option (#905)

--- internal/codegen/golang/field.go
+++ internal/codegen/golang/field.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+
+	"github.com/kyleconroy/sqlc/internal/config"
 )
 
 type Field struct {
@@ -23,4 +25,54 @@ func (gf Field) Tag() string {
 	}
 	sort.Strings(tags)
 	return strings.Join(tags, " ")
+}
+
+func JSONTagName(name string, settings config.CombinedSettings) string {
+	style := settings.Go.JSONTagsCaseStyle
+	if style == "" || style == "none" {
+		return name
+	} else {
+		return SetCaseStyle(name, style)
+	}
+}
+
+func SetCaseStyle(name string, style string) string {
+	switch style {
+	case "camel":
+		return toCamelCase(name)
+	case "pascal":
+		return toPascalCase(name)
+	case "snake":
+		return toSnakeCase(name)
+	default:
+		panic(fmt.Sprintf("unsupported JSON tags case style: '%s'", style))
+	}
+}
+
+func toSnakeCase(s string) string {
+	return strings.ToLower(s)
+}
+
+func toCamelCase(s string) string {
+	return toCamelInitCase(s, false)
+}
+
+func toPascalCase(s string) string {
+	return toCamelInitCase(s, true)
+}
+
+func toCamelInitCase(name string, initUpper bool) string {
+	out := ""
+	for i, p := range strings.Split(name, "_") {
+		if !initUpper && i == 0 {
+			out += p
+			continue
+		}
+		if p == "id" {
+			out += "ID"
+		} else {
+			out += strings.Title(p)
+		}
+	}
+	return out
 }
