7eed90db8fab628088406ddbf55d07aad65b2fba
Robert Holt <rholt@datto.com>
2021-03-16 11:57:51 -0400

    Python: Eliminate runtime package and use sqlalchemy (#939)
    * Manually edit python example code
    
    The generated example code is edited just to demostrate potential
    improvements to the python codegen.
    
    The main changes are using sqlalchemy as the exection engine and using
    stdlib dataclasses instead of pydantic. This also eliminates the runtime
    package.
    
    * Remove leftover debug code in python tests
    
    * Python: implement new codegen
    
    Generated python code now only depends on sqlalchemy. Query functions
    are now inside classes as well.

--- examples/python/src/dbtest/migrations.py
+++ examples/python/src/dbtest/migrations.py
@@ -1,29 +1,31 @@
 import os
 from typing import List
 
-import asyncpg
-import psycopg2.extensions
+import sqlalchemy
+import sqlalchemy.ext.asyncio
 
 
-def apply_migrations(db: psycopg2.extensions.connection, paths: List[str]):
+def apply_migrations(conn: sqlalchemy.engine.Connection, paths: List[str]):
     files = _find_sql_files(paths)
 
     for file in files:
         with open(file, "r") as fd:
             blob = fd.read()
-        cur = db.cursor()
-        cur.execute(blob)
-        cur.close()
-        db.commit()
+        stmts = blob.split(";")
+        for stmt in stmts:
+            if stmt.strip():
+                conn.execute(sqlalchemy.text(stmt))
 
 
-async def apply_migrations_async(db: asyncpg.Connection, paths: List[str]):
+async def apply_migrations_async(conn: sqlalchemy.ext.asyncio.AsyncConnection, paths: List[str]):
     files = _find_sql_files(paths)
 
     for file in files:
         with open(file, "r") as fd:
             blob = fd.read()
-        await db.execute(blob)
+        raw_conn = await conn.get_raw_connection()
+        # The asyncpg sqlalchemy adapter uses a prepared statement cache which can't handle the migration statements
+        await raw_conn._connection.execute(blob)
 
 
 def _find_sql_files(paths: List[str]) -> List[str]:
