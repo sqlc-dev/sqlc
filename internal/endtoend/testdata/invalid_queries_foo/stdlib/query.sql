aa1c553bb9788fa5d2cc195f98b9a4a70d0dedc2
Victor Lima <victoraugustolls@users.noreply.github.com>
2021-05-23 22:37:48 -0300

    tests: added invalid_queries_foo pgx test

---
+++ internal/endtoend/testdata/invalid_queries_foo/stdlib/query.sql
@@ -0,0 +1,19 @@
+CREATE TABLE foo (id text not null);
+
+-- name: ListFoos
+SELECT id FROM foo;
+
+-- name: ListFoos :one :many
+SELECT id FROM foo;
+
+-- name: ListFoos :two
+SELECT id FROM foo;
+
+-- name: DeleteFoo :one
+DELETE FROM foo WHERE id = $1;
+
+-- name: UpdateFoo :one
+UPDATE foo SET id = $2 WHERE id = $1;
+
+-- name: InsertFoo :one
+INSERT INTO foo (id) VALUES ($1);
