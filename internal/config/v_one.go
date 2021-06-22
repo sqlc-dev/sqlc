947c430a91cfb83059a14f35436d0de1bf9ca362
Leandro Souza <44624358+Leanndrosouza@users.noreply.github.com>
2021-06-22 13:19:06 -0300

    Export generated query for telemetry usage (#1054)
    * add emit_exported_queries to config
    
    * change capitalization of query var name based on EmitExportedQueries
    
    * create tests for emit exported queries on pgx and stdlib drivers

--- internal/config/v_one.go
+++ internal/config/v_one.go
@@ -27,6 +27,7 @@ type v1PackageSettings struct {
 	EmitPreparedQueries   bool       `json:"emit_prepared_queries" yaml:"emit_prepared_queries"`
 	EmitExactTableNames   bool       `json:"emit_exact_table_names,omitempty" yaml:"emit_exact_table_names"`
 	EmitEmptySlices       bool       `json:"emit_empty_slices,omitempty" yaml:"emit_empty_slices"`
+	EmitExportedQueries   bool       `json:"emit_exported_queries,omitempty" yaml:"emit_exported_queries"`
 	JSONTagsCaseStyle     string     `json:"json_tags_case_style,omitempty" yaml:"json_tags_case_style"`
 	Driver                string     `json:"driver" yaml:"driver"`
 	Overrides             []Override `json:"overrides" yaml:"overrides"`
@@ -115,6 +116,7 @@ func (c *V1GenerateSettings) Translate() Config {
 					EmitPreparedQueries:   pkg.EmitPreparedQueries,
 					EmitExactTableNames:   pkg.EmitExactTableNames,
 					EmitEmptySlices:       pkg.EmitEmptySlices,
+					EmitExportedQueries:   pkg.EmitExportedQueries,
 					Package:               pkg.Name,
 					Out:                   pkg.Path,
 					Driver:                pkg.Driver,
