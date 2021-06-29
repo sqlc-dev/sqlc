77f0e1008f95bc0381092409babd1bb8c78c2c59
Øyvind Størkersen <oyvind@strk.no>
2021-06-29 22:49:37 +0200

    fix for index out of range [0] in codegen.LowerTitle when input is empty

---
+++ internal/codegen/utils_test.go
@@ -0,0 +1,28 @@
+package codegen
+
+import (
+	"testing"
+)
+
+func TestLowerTitle(t *testing.T) {
+
+	// empty
+	if LowerTitle("") != "" {
+		t.Fatal("expected empty title to remain empty")
+	}
+
+	// all lowercase
+	if LowerTitle("lowercase") != "lowercase" {
+		t.Fatal("expected no changes when input is all lowercase")
+	}
+
+	// all uppercase
+	if LowerTitle("UPPERCASE") != "uPPERCASE" {
+		t.Fatal("expected first rune to be lower when input is all uppercase")
+	}
+
+	// Title Case
+	if LowerTitle("Title Case") != "title Case" {
+		t.Fatal("expected first rune to be lower when input is Title Case")
+	}
+}
