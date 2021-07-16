sqlc-dev/sqlc

commit 885bd3d3191354f3d9f081fcf7fbd2928b55f78b

Make seen use fieldName instead of colName.
fieldName is after any mangilng to make it fit go variable standards,
and since that is the name that we are actually using, that is the name
that we should be checking for conflicts.

This only matters when we have multiple fields that differ only by
things that are changed when we are converting to the go variable
standards.

internal/codegen/golang/result.go
@@ -252,7 +252,7 @@ func columnsToStruct(r *compiler.Result, name string, columns []goColumn, settin
 		suffix := 0
 		if o, ok := suffixes[c.id]; ok && useID {
 			suffix = o
-		} else if v := seen[colName]; v > 0 {
+		} else if v := seen[fieldName]; v > 0 {
 			suffix = v + 1
 		}
 		suffixes[c.id] = suffix
@@ -272,7 +272,8 @@ func columnsToStruct(r *compiler.Result, name string, columns []goColumn, settin
 			Type: goType(r, c.Column, settings),
 			Tags: tags,
 		})
-		seen[colName]++
+		seen[fieldName]++
 	}
+
 	return &gs
 }

commit bae3d66037b0421d65b8bb90e09dbcfaec79a2fd

Make seen use the pre-modification fieldName.
Oops, this fixes a bug introduced in
885bd3d3191354f3d9f081fcf7fbd2928b55f78b.

internal/codegen/golang/result.go
@@ -258,6 +258,7 @@ func columnsToStruct(r *compiler.Result, name string, columns []goColumn, settin
 		colName := columnName(c.Column, i)
 		tagName := colName
 		fieldName := StructName(colName, settings)
+		baseFieldName := fieldName
 		// Track suffixes by the ID of the column, so that columns referring to the same numbered parameter can be
 		// reused.
 		suffix := 0
@@ -283,7 +284,7 @@ func columnsToStruct(r *compiler.Result, name string, columns []goColumn, settin
 			Type: goType(r, c.Column, settings),
 			Tags: tags,
 		})
-		seen[fieldName]++
+		seen[baseFieldName]++
 	}
 
 	return &gs