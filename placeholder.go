d446b81ad9c02b95aab40813b7a061f6b1e99ae5
oliverpool <3864879+oliverpool@users.noreply.github.com>
2021-09-21 10:40:33 +0200

    Exportable Run function
    Closes #416

--- placeholder.go
+++ placeholder.go
@@ -1,6 +1,17 @@
 package sqlc
 
+import (
+	"io"
+
+	"github.com/kyleconroy/sqlc/internal/cmd"
+)
+
 // This is a dummy file that allows SQLC to be "installed" as a module and locked using
 // go.mod and then run using "go run github.com/kyleconroy/sqlc"
 
 type Placeholder struct{}
+
+// Run the sqlc command
+func Run(args []string, stdin io.Reader, stdout io.Writer, stderr io.Writer) int {
+	return cmd.Do(args, stdin, stdout, stderr)
+}
