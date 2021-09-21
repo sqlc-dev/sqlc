6cea18db4b213a762b7e59b24d906c972cb1da9d
oliverpool <3864879+oliverpool@users.noreply.github.com>
2021-09-21 17:39:09 +0200

    add pkg/cli package (#1204)
    Closes #416

---
+++ pkg/cli/cli.go
@@ -0,0 +1,31 @@
+// package cli exposes the command-line interface for sqlc. It can be used to
+// run sqlc from Go without the overhead of creating a child process.
+//
+// Example usage:
+//
+//     package main
+//
+//     import (
+//         "os"
+//
+//         sqlc "github.com/kyleconroy/sqlc/pkg/cli"
+//     )
+//
+//     func main() {
+//         os.Exit(sqlc.Run(os.Args[1:]))
+//     }
+//
+package cli
+
+import (
+	"os"
+
+	"github.com/kyleconroy/sqlc/internal/cmd"
+)
+
+// Run the sqlc CLI. It takes an array of command-line arguments
+// (excluding the executable argument itself) and returns an exit
+// code.
+func Run(args []string) int {
+	return cmd.Do(args, os.Stdin, os.Stdout, os.Stderr)
+}
