7bbc905f0850735504c477ed6d3cd4f4ac079da8
Kyle Conroy <kyle@conroy.org>
2021-09-23 08:16:28 -0700

    fix(engine/postgresql): jsonb_build_object
    Update the function signature of jsonb_build_object and
    jsonb_build_array with variadic arguments.

--- internal/endtoend/testdata/json_build/postgresql/pgx/go/query.sql.go
+++ internal/endtoend/testdata/json_build/postgresql/pgx/go/query.sql.go
@@ -9,6 +9,66 @@ import (
 	"github.com/jackc/pgtype"
 )
 
+const selectJSONBBuildArray = `-- name: SelectJSONBBuildArray :one
+SELECT 
+  jsonb_build_array(),
+  jsonb_build_array(1),
+  jsonb_build_array(1, 2),
+  jsonb_build_array(1, 2, 'foo'),
+  jsonb_build_array(1, 2, 'foo', 4)
+`
+
+type SelectJSONBBuildArrayRow struct {
+	JsonbBuildArray   pgtype.JSONB
+	JsonbBuildArray_2 pgtype.JSONB
+	JsonbBuildArray_3 pgtype.JSONB
+	JsonbBuildArray_4 pgtype.JSONB
+	JsonbBuildArray_5 pgtype.JSONB
+}
+
+func (q *Queries) SelectJSONBBuildArray(ctx context.Context) (SelectJSONBBuildArrayRow, error) {
+	row := q.db.QueryRow(ctx, selectJSONBBuildArray)
+	var i SelectJSONBBuildArrayRow
+	err := row.Scan(
+		&i.JsonbBuildArray,
+		&i.JsonbBuildArray_2,
+		&i.JsonbBuildArray_3,
+		&i.JsonbBuildArray_4,
+		&i.JsonbBuildArray_5,
+	)
+	return i, err
+}
+
+const selectJSONBBuildObject = `-- name: SelectJSONBBuildObject :one
+SELECT
+  jsonb_build_object(),
+  jsonb_build_object('foo'),
+  jsonb_build_object('foo', 1),
+  jsonb_build_object('foo', 1, 2),
+  jsonb_build_object('foo', 1, 2, 'bar')
+`
+
+type SelectJSONBBuildObjectRow struct {
+	JsonbBuildObject   pgtype.JSONB
+	JsonbBuildObject_2 pgtype.JSONB
+	JsonbBuildObject_3 pgtype.JSONB
+	JsonbBuildObject_4 pgtype.JSONB
+	JsonbBuildObject_5 pgtype.JSONB
+}
+
+func (q *Queries) SelectJSONBBuildObject(ctx context.Context) (SelectJSONBBuildObjectRow, error) {
+	row := q.db.QueryRow(ctx, selectJSONBBuildObject)
+	var i SelectJSONBBuildObjectRow
+	err := row.Scan(
+		&i.JsonbBuildObject,
+		&i.JsonbBuildObject_2,
+		&i.JsonbBuildObject_3,
+		&i.JsonbBuildObject_4,
+		&i.JsonbBuildObject_5,
+	)
+	return i, err
+}
+
 const selectJSONBuildArray = `-- name: SelectJSONBuildArray :one
 SELECT 
   json_build_array(),
