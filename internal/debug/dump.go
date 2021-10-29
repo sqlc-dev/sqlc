63a755da630a64ea4b04b67ef4fcab85e58cc639
Kyle Conroy <kyle@conroy.org>
2021-10-29 09:44:15 -0700

    feat: Instrument compiler via runtime/trace (#1258)
    * feat: Instrument compiler via runtime/trace

--- internal/debug/dump.go
+++ internal/debug/dump.go
@@ -4,12 +4,20 @@ import (
 	"os"
 
 	"github.com/davecgh/go-spew/spew"
+
+	"github.com/kyleconroy/sqlc/internal/opts"
 )
 
 var Active bool
+var Traced bool
+var Debug opts.Debug
 
 func init() {
 	Active = os.Getenv("SQLCDEBUG") != ""
+	if Active {
+		Debug = opts.DebugFromEnv()
+		Traced = Debug.Trace != ""
+	}
 }
 
 func Dump(n ...interface{}) {
