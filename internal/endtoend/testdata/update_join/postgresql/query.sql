b971a2824452041bc9860d499a151c9bfb9b50c0
Timothy Studd <timstudd@gmail.com>
2021-11-16 05:08:45 -0800

    Fix formatting

--- internal/endtoend/testdata/update_join/postgresql/query.sql
+++ internal/endtoend/testdata/update_join/postgresql/query.sql
@@ -16,4 +16,4 @@ SET     is_active = $1
 FROM    primary_table
 WHERE   join_table.id = $2
         AND primary_table.user_id = $3
-		AND join_table.primary_table_id = primary_table.id;
+        AND join_table.primary_table_id = primary_table.id;
