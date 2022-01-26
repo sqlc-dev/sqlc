af586a19531efdf6e0bdcf82422820b8d1b9ff58
Padraig <padraig.kennedy@gmail.com>
2022-01-26 11:33:47 -0800

    Handle select nodes

--- internal/engine/sqlite/convert.go
+++ internal/engine/sqlite/convert.go
@@ -101,8 +101,7 @@ func convertExprContext(c *parser.ExprContext) ast.Node {
 	return &ast.TODO{}
 }
 
-/*
-func convertFactored_select_stmtContext(c *parser.Factored_select_stmtContext) ast.Node {
+func convertSelect_stmtContext(c *parser.Select_stmtContext) ast.Node {
 	var tables []ast.Node
 	var cols []ast.Node
 	for _, icore := range c.AllSelect_core() {
@@ -110,57 +109,99 @@ func convertFactored_select_stmtContext(c *parser.Factored_select_stmtContext) a
 		if !ok {
 			continue
 		}
-		for _, icol := range core.AllResult_column() {
-			col, ok := icol.(*parser.Result_columnContext)
-			if !ok {
-				continue
-			}
-			var val ast.Node
-			iexpr := col.Expr()
-			switch {
-			case col.STAR() != nil:
-				val = &ast.ColumnRef{
-					Fields: &ast.List{
-						Items: []ast.Node{
-							&ast.A_Star{},
-						},
-					},
-					Location: col.GetStart().GetStart(),
-				}
-			case iexpr != nil:
-				val = convert(iexpr)
-			}
-			if val == nil {
-				continue
-			}
-			cols = append(cols, &ast.ResTarget{
-				Val:      val,
-				Location: col.GetStart().GetStart(),
-			})
-		}
-		for _, ifrom := range core.AllTable_or_subquery() {
-			from, ok := ifrom.(*parser.Table_or_subqueryContext)
-			if !ok {
-				continue
-			}
-			rel := from.Table_name().GetText()
-			name := ast.RangeVar{
-				Relname:  &rel,
-				Location: from.GetStart().GetStart(),
-			}
-			if from.Schema_name() != nil {
-				text := from.Schema_name().GetText()
-				name.Schemaname = &text
-			}
-			tables = append(tables, &name)
-		}
+		cols = append(cols, getCols(core)...)
+		tables = append(cols, getTables(core)...)
 	}
 	return &ast.SelectStmt{
 		FromClause: &ast.List{Items: tables},
 		TargetList: &ast.List{Items: cols},
 	}
 }
-*/
+
+func convertSimpleSelect_stmtContext(c *parser.Simple_select_stmtContext) ast.Node {
+	if core, ok := c.Select_core().(*parser.Select_coreContext); ok {
+		cols := getCols(core)
+		tables := getTables(core)
+
+		return &ast.SelectStmt{
+			FromClause: &ast.List{Items: tables},
+			TargetList: &ast.List{Items: cols},
+		}
+	}
+
+	return &ast.TODO{}
+}
+
+func convertCompoundSelect_stmtContext(c *parser.Compound_select_stmtContext) ast.Node {
+	var tables []ast.Node
+	var cols []ast.Node
+	for _, icore := range c.AllSelect_core() {
+		core, ok := icore.(*parser.Select_coreContext)
+		if !ok {
+			continue
+		}
+		cols = append(cols, getCols(core)...)
+		tables = append(cols, getTables(core)...)
+	}
+	return &ast.SelectStmt{
+		FromClause: &ast.List{Items: tables},
+		TargetList: &ast.List{Items: cols},
+	}
+}
+
+func getTables(core *parser.Select_coreContext) []ast.Node {
+	var tables []ast.Node
+	for _, ifrom := range core.AllTable_or_subquery() {
+		from, ok := ifrom.(*parser.Table_or_subqueryContext)
+		if !ok {
+			continue
+		}
+		rel := from.Table_name().GetText()
+		name := ast.RangeVar{
+			Relname:  &rel,
+			Location: from.GetStart().GetStart(),
+		}
+		if from.Schema_name() != nil {
+			text := from.Schema_name().GetText()
+			name.Schemaname = &text
+		}
+		tables = append(tables, &name)
+	}
+	return tables
+}
+
+func getCols(core *parser.Select_coreContext) []ast.Node {
+	var cols []ast.Node
+	for _, icol := range core.AllResult_column() {
+		col, ok := icol.(*parser.Result_columnContext)
+		if !ok {
+			continue
+		}
+		var val ast.Node
+		iexpr := col.Expr()
+		switch {
+		case col.STAR() != nil:
+			val = &ast.ColumnRef{
+				Fields: &ast.List{
+					Items: []ast.Node{
+						&ast.A_Star{},
+					},
+				},
+				Location: col.GetStart().GetStart(),
+			}
+		case iexpr != nil:
+			val = convert(iexpr)
+		}
+		if val == nil {
+			continue
+		}
+		cols = append(cols, &ast.ResTarget{
+			Val:      val,
+			Location: col.GetStart().GetStart(),
+		})
+	}
+	return cols
+}
 
 func convertSql_stmtContext(n *parser.Sql_stmtContext) ast.Node {
 	if stmt := n.Alter_table_stmt(); stmt != nil {
@@ -259,18 +300,18 @@ func convert(node node) ast.Node {
 	case *parser.Factored_select_stmtContext:
 		// TODO: need to handle this
 		return &ast.TODO{}
-	//	return convertFactored_select_stmtContext(n)
+
+	case *parser.Select_stmtContext:
+		return convertSelect_stmtContext(n)
 
 	case *parser.Sql_stmtContext:
 		return convertSql_stmtContext(n)
 
 	case *parser.Simple_select_stmtContext:
-		// TODO: need to handle this
-		return &ast.TODO{}
+		return convertSimpleSelect_stmtContext(n)
 
 	case *parser.Compound_select_stmtContext:
-		// TODO: need to handle this
-		return &ast.TODO{}
+		return convertCompoundSelect_stmtContext(n)
 
 	default:
 		return &ast.TODO{}
