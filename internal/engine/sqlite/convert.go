8eaf56adb08388df246442ea1142d21c8b812b22
Padraig <padraig.kennedy@gmail.com>
2022-02-02 11:30:45 -0800

    Add missing import

--- internal/engine/sqlite/convert.go
+++ internal/engine/sqlite/convert.go
@@ -2,6 +2,7 @@ package sqlite
 
 import (
 	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"strings"
 
 	"github.com/kyleconroy/sqlc/internal/engine/sqlite/parser"
 	"github.com/kyleconroy/sqlc/internal/sql/ast"
