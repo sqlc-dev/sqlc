26a09179e03d119dd375b545ab38e2d29884c59c
Jon Perritt <jrperritt@gmail.com>
2022-02-13 14:41:12 -0600

    batch* example

---
+++ examples/batch/postgresql/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE authors (
+          author_id SERIAL PRIMARY KEY,
+          name text NOT NULL DEFAULT ''
+);
+
+CREATE TYPE book_type AS ENUM (
+          'FICTION',
+          'NONFICTION'
+);
+
+CREATE TABLE books (
+          book_id SERIAL PRIMARY KEY,
+          author_id integer NOT NULL REFERENCES authors(author_id),
+          isbn text NOT NULL DEFAULT '' UNIQUE,
+          book_type book_type NOT NULL DEFAULT 'FICTION',
+          title text NOT NULL DEFAULT '',
+          year integer NOT NULL DEFAULT 2000,
+          available timestamp with time zone NOT NULL DEFAULT 'NOW()',
+          tags varchar[] NOT NULL DEFAULT '{}'
+);
