ce73dece1369d3b1b038d7831040ee08bfc09a8c
Alex Dulin <alex@morningconsult.com>
2022-02-14 11:19:38 -0500

    chore: fix extra newline in comments for copyfrom
    The changes in #1417 fixed newline comments in all queries except for copyfrom.
    This PR adds the same changes for copyfrom so generated comments do not have
    a new line before the function.
    
    Signed-off-by: Alex Dulin <alex@morningconsult.com>

--- internal/endtoend/testdata/copyfrom/postgresql/pgx/query.sql
+++ internal/endtoend/testdata/copyfrom/postgresql/pgx/query.sql
@@ -2,7 +2,9 @@ CREATE SCHEMA myschema;
 CREATE TABLE myschema.foo (a text, b integer);
 
 -- name: InsertValues :copyfrom
+-- InsertValues inserts multiple values using copy.
 INSERT INTO myschema.foo (a, b) VALUES ($1, $2);
 
 -- name: InsertSingleValue :copyfrom
+-- InsertSingleValue inserts a single value using copy.
 INSERT INTO myschema.foo (a) VALUES ($1);
