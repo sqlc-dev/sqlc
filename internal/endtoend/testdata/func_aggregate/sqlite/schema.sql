81717e96087c770d13fd40d76b1a67fc9262949c
Padraig <padraig.kennedy@gmail.com>
2022-02-20 12:02:29 -0800

    Add initial SQLite aggregate func test

---
+++ internal/endtoend/testdata/func_aggregate/sqlite/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books (
+                       id   integer PRIMARY KEY,
+                       title text      NOT NULL,
+                       author text     NOT NULL,
+                       pages integer      NOT NULL
+
+);
