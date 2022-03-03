9bed9df9a0b090c6a0b88773fb6b85a682a57e26
Kyle Conroy <kyle@conroy.org>
2022-03-03 09:42:49 -0800

    docs: Add new logo

--- docs/conf.py
+++ docs/conf.py
@@ -54,3 +54,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = "_static/logo.png"
+html_theme_options = {
+    'logo_only': True,
+}
+
+def setup(app):
+    app.add_css_file('customize.css')
