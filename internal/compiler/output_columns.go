4a72269808dc1e58031f8b40f4b16ff59f22a22f
Richard Bizik <richard.bizik@gmail.com>
2022-03-03 18:52:43 +0100

    Removed ast modifications, replaced with nullableNodes
    nullableNodes are used to identify their children and
    set columns to nullable

--- internal/compiler/output_columns.go
+++ internal/compiler/output_columns.go
@@ -358,6 +358,7 @@ func isTableRequired(n ast.Node, col *Column, prior int) int {
 // Return an error if an unknown column is referenced
 func sourceTables(qc *QueryCatalog, node ast.Node) ([]*Table, error) {
 	var list *ast.List
+	var nullableNodes []*ast.Node
 	switch n := node.(type) {
 	case *ast.DeleteStmt:
 		list = &ast.List{
@@ -368,14 +369,29 @@ func sourceTables(qc *QueryCatalog, node ast.Node) ([]*Table, error) {
 			Items: []ast.Node{n.Relation},
 		}
 	case *ast.SelectStmt:
-		list = astutils.Search(n.FromClause, func(node ast.Node) bool {
-			switch node.(type) {
+		dirtyList := astutils.Search(n.FromClause, func(node ast.Node) bool {
+			switch t := node.(type) {
 			case *ast.RangeVar, *ast.RangeSubselect, *ast.FuncName:
 				return true
+			case *ast.JoinExpr:
+				if t.Jointype == ast.JoinTypeLeft || t.Jointype == ast.JoinTypeFull {
+					return true
+				}
+				return false
 			default:
 				return false
 			}
 		})
+		// split result into nullableJoins and list of nodes to handle
+		list = &ast.List{}
+		for i, v := range dirtyList.Items {
+			switch t := dirtyList.Items[i].(type) {
+			case *ast.JoinExpr:
+				nullableNodes = append(nullableNodes, &t.Rarg)
+			default:
+				list.Items = append(list.Items, v)
+			}
+		}
 	case *ast.TruncateStmt:
 		list = astutils.Search(n.Relations, func(node ast.Node) bool {
 			_, ok := node.(*ast.RangeVar)
@@ -412,17 +428,14 @@ func sourceTables(qc *QueryCatalog, node ast.Node) ([]*Table, error) {
 
 		case *ast.RangeSubselect:
 			cols, err := outputColumns(qc, n.Subquery)
-			// update columns if we are joining subquery
-			switch n.JoinType {
-			case ast.JoinTypeLeft:
-				fallthrough
-			case ast.JoinTypeFull:
+
+			if err != nil {
+				return nil, err
+			}
+			if astutils.IsChildOfNodes(nullableNodes, &item) {
 				for _, c := range cols {
 					c.NotNull = false
 				}
-			}
-			if err != nil {
-				return nil, err
 			}
 			tables = append(tables, &Table{
 				Rel: &ast.TableName{
