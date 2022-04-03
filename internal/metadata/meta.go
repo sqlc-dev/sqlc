2d8892b46a87354e1ff95fef679c6b11f8314332
David Pennington <56460+Xeoncross@users.noreply.github.com>
2022-04-03 11:06:50 -0500

    Adding LastInsertId for Go (#1484)
    * adding :execlastinsertid for golang

--- internal/metadata/meta.go
+++ internal/metadata/meta.go
@@ -16,6 +16,7 @@ const (
 	CmdExec       = ":exec"
 	CmdExecResult = ":execresult"
 	CmdExecRows   = ":execrows"
+	CmdExecLastId = ":execlastid"
 	CmdMany       = ":many"
 	CmdOne        = ":one"
 	CmdCopyFrom   = ":copyfrom"
@@ -83,7 +84,7 @@ func Parse(t string, commentStyle CommentSyntax) (string, string, error) {
 			part = part[:len(part)-1] // removes the trailing "*/" element
 		}
 		if len(part) == 2 {
-			return "", "", fmt.Errorf("missing query type [':one', ':many', ':exec', ':execrows', ':execresult', ':copyfrom', 'batchexec', 'batchmany', 'batchone']: %s", line)
+			return "", "", fmt.Errorf("missing query type [':one', ':many', ':exec', ':execrows', ':execlastid', ':execresult', ':copyfrom', 'batchexec', 'batchmany', 'batchone']: %s", line)
 		}
 		if len(part) != 4 {
 			return "", "", fmt.Errorf("invalid query comment: %s", line)
@@ -91,7 +92,7 @@ func Parse(t string, commentStyle CommentSyntax) (string, string, error) {
 		queryName := part[2]
 		queryType := strings.TrimSpace(part[3])
 		switch queryType {
-		case CmdOne, CmdMany, CmdExec, CmdExecResult, CmdExecRows, CmdCopyFrom, CmdBatchExec, CmdBatchMany, CmdBatchOne:
+		case CmdOne, CmdMany, CmdExec, CmdExecResult, CmdExecRows, CmdExecLastId, CmdCopyFrom, CmdBatchExec, CmdBatchMany, CmdBatchOne:
 		default:
 			return "", "", fmt.Errorf("invalid query type: %s", queryType)
 		}
