de0923599b2d16d7284df978b006ee3a2245d3b2
Kazuyuki Honda <hakobera@gmail.com>
2022-06-12 01:29:39 +0900

    add booktest example for SQLite

---
+++ examples/booktest/sqlite/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE authors (
+          author_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+          name text NOT NULL
+);
+
+CREATE INDEX authors_name_idx ON authors(name);
+
+CREATE TABLE books (
+          book_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+          author_id integer NOT NULL,
+          isbn varchar(255) NOT NULL DEFAULT '' UNIQUE,
+          book_type text NOT NULL DEFAULT 'FICTION',
+          title text NOT NULL,
+          yr integer NOT NULL DEFAULT 2000,
+          available datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          tags text NOT NULL,
+          CHECK (book_type = 'FICTION' OR book_type = 'NONFICTION')
+);
+
+CREATE INDEX books_title_idx ON books(title, yr);
