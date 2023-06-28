7fd89bcb4ddbb17a5165172e3b0b510a3baace61
Kyle Conroy <kyle@sqlc.dev>
2023-06-28 15:34:25 -0700

    feat(cmd/vet): Prepare queries for MySQL (#2388)
    * feat(cmd/vet): Prepare statements for MySQL
    * fix: Return an error unsupported engines
    * chore: Use PostgreSQL 15 and MySQL 8
    * chore: Fix new MySQL image

--- internal/sqltest/postgres.go
+++ internal/sqltest/postgres.go
@@ -28,87 +28,13 @@ func id() string {
 	return string(b)
 }
 
-// Disable random new schema
-// Override database name
-func CreatePostgreSQLDatabase(t *testing.T, newDB string, migrations []string) *sql.DB {
-	t.Helper()
-
-	pgUser := os.Getenv("PG_USER")
-	pgHost := os.Getenv("PG_HOST")
-	pgPort := os.Getenv("PG_PORT")
-	pgPass := os.Getenv("PG_PASSWORD")
-	pgDB := os.Getenv("PG_DATABASE")
-
-	if pgUser == "" {
-		pgUser = "postgres"
-	}
-
-	if pgPass == "" {
-		pgPass = "mysecretpassword"
-	}
-
-	if pgPort == "" {
-		pgPort = "5432"
-	}
-
-	if pgHost == "" {
-		pgHost = "127.0.0.1"
-	}
-
-	if pgDB == "" {
-		pgDB = "dinotest"
-	}
-
-	source := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", pgUser, pgPass, pgHost, pgPort, pgDB)
-	t.Logf("db: %s", source)
-
-	db, err := sql.Open("postgres", source)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer db.Close()
-
-	var exists bool
-	dberr := db.QueryRow(`SELECT true FROM pg_database WHERE datname = $1`, newDB).Scan(&exists)
-	if dberr != nil && dberr != sql.ErrNoRows {
-		t.Fatal(err)
-	}
-
-	if !exists {
-		if _, err := db.Exec("CREATE DATABASE " + newDB); err != nil {
-			t.Fatal(err)
-		}
-	} else {
-		t.Logf("database '%s' exists, not creating", newDB)
-	}
-
-	newSource := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", pgUser, pgPass, pgHost, pgPort, newDB)
-	t.Logf("newdb: %s", newSource)
-
-	sdb, err := sql.Open("postgres", newSource)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !exists {
-		files, err := sqlpath.Glob(migrations)
-		if err != nil {
-			t.Fatal(err)
-		}
-		for _, f := range files {
-			blob, err := os.ReadFile(f)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if _, err := sdb.Exec(string(blob)); err != nil {
-				t.Fatalf("%s: %s", filepath.Base(f), err)
-			}
-		}
-	}
-	return sdb
+func PostgreSQL(t *testing.T, migrations []string) (*sql.DB, func()) {
+	// For each test, pick a new schema name at random.
+	schema := "sqltest_postgresql_" + id()
+	return CreatePostgreSQLDatabase(t, schema, true, migrations)
 }
 
-func PostgreSQL(t *testing.T, migrations []string) (*sql.DB, func()) {
+func CreatePostgreSQLDatabase(t *testing.T, name string, schema bool, migrations []string) (*sql.DB, func()) {
 	t.Helper()
 
 	pgUser := os.Getenv("PG_USER")
@@ -146,12 +72,22 @@ func PostgreSQL(t *testing.T, migrations []string) (*sql.DB, func()) {
 	}
 
 	// For each test, pick a new schema name at random.
-	schema := "sqltest_postgresql_" + id()
-	if _, err := db.Exec("CREATE SCHEMA " + schema); err != nil {
-		t.Fatal(err)
+	var newsource, dropQuery string
+	if schema {
+		if _, err := db.Exec("CREATE SCHEMA " + name); err != nil {
+			t.Fatal(err)
+		}
+		newsource = source + "&search_path=" + name
+		dropQuery = "DROP SCHEMA " + name + " CASCADE"
+	} else {
+		if _, err := db.Exec("CREATE DATABASE " + name); err != nil {
+			t.Fatal(err)
+		}
+		newsource = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", pgUser, pgPass, pgHost, pgPort, name)
+		dropQuery = "DROP DATABASE IF EXISTS " + name + " WITH (FORCE)"
 	}
 
-	sdb, err := sql.Open("postgres", source+"&search_path="+schema)
+	sdb, err := sql.Open("postgres", newsource)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -171,8 +107,9 @@ func PostgreSQL(t *testing.T, migrations []string) (*sql.DB, func()) {
 	}
 
 	return sdb, func() {
-		if _, err := db.Exec("DROP SCHEMA " + schema + " CASCADE"); err != nil {
+		if _, err := db.Exec(dropQuery); err != nil {
 			t.Fatal(err)
 		}
+		db.Close()
 	}
 }
