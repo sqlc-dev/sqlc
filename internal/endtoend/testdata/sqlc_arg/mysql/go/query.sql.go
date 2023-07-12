f5c528fe267b9c64476ab0b21812023786a8a6b0
Andrew Benton <andrew@sqlc.dev>
2023-07-12 17:12:11 -0400

    Add an endtoend test that covers the case referenced in https://github.com/kyleconroy/sqlc/issues/2403 (#2442)

--- internal/endtoend/testdata/sqlc_arg/mysql/go/query.sql.go
+++ internal/endtoend/testdata/sqlc_arg/mysql/go/query.sql.go
@@ -9,6 +9,38 @@ import (
 	"context"
 )
 
+const complicated = `-- name: Complicated :many
+WITH names AS (SELECT name from foo WHERE foo.name = ?)
+SELECT name FROM names WHERE name IN (SELECT name FROM foo WHERE foo.name = ?)
+`
+
+type ComplicatedParams struct {
+	Slug string
+}
+
+func (q *Queries) Complicated(ctx context.Context, arg ComplicatedParams) ([]string, error) {
+	rows, err := q.db.QueryContext(ctx, complicated, arg.Slug, arg.Slug)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []string
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return nil, err
+		}
+		items = append(items, name)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const funcParamIdent = `-- name: FuncParamIdent :many
 SELECT name FROM foo WHERE name = ?
 `
