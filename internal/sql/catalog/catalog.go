28ce2c9d49a11c6504086440b11da6894e86bdc2
Kyle Conroy <kyle@sqlc.dev>
2023-07-28 15:38:32 -0700

    Remove debug statements

--- internal/sql/catalog/catalog.go
+++ internal/sql/catalog/catalog.go
@@ -46,7 +46,6 @@ func (c *Catalog) Update(stmt ast.Statement, colGen columnGenerator) error {
 	if stmt.Raw == nil {
 		return nil
 	}
-	debug.Dump("update", stmt)
 	var err error
 	switch n := stmt.Raw.Stmt.(type) {
 
