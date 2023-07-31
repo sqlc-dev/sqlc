98ef71db35880c939855b971a8be979746085283
Kyle Gray <kyle@sqlc.dev>
2023-07-31 11:10:34 -0700

    feat(cmd/sqlc): Bump version to 1.20.0 (#2549)
    * feat(cmd/sqlc): Bump version to 1.20.0
    
    ---------
    
    Co-authored-by: Andrew Benton <andrew@sqlc.dev>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Riza, Inc.'
 author = 'Riza, Inc.'
 
 # The full version, including alpha/beta/rc tags
-release = '1.19.1'
+release = '1.20.0'
 
 
 # -- General configuration ---------------------------------------------------
