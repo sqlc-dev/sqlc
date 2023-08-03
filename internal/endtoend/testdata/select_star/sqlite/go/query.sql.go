655f2d19df04ba5a8f66daa13c80bf1b4ca31b6c
Nao Yonashiro <owan.orisano@gmail.com>
2023-08-04 00:27:18 +0900

    test: add endtoend

--- internal/endtoend/testdata/select_star/sqlite/go/query.sql.go
+++ internal/endtoend/testdata/select_star/sqlite/go/query.sql.go
@@ -40,3 +40,30 @@ func (q *Queries) GetAll(ctx context.Context) ([]User, error) {
 	}
 	return items, nil
 }
+
+const getIDAll = `-- name: GetIDAll :many
+SELECT id FROM (SELECT id FROM users) t
+`
+
+func (q *Queries) GetIDAll(ctx context.Context) ([]int64, error) {
+	rows, err := q.db.QueryContext(ctx, getIDAll)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []int64
+	for rows.Next() {
+		var id int64
+		if err := rows.Scan(&id); err != nil {
+			return nil, err
+		}
+		items = append(items, id)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
