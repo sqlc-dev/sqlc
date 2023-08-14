ce902f1dcbd5c87c412c878036fc82183535b426
Jille Timmermans <jille@quis.cx>
2023-08-14 21:57:45 +0200

    Remove PrepareContext from DBTX unless emit_prepared_queries: true
    sqlc's generated code doesn't use that method (unless
    emit_prepared_queries is enabled). Let's exclude it from the generated
    interface so that people providing a custom DBTX don't have to implement
    a unused function.
    
    This is a minor backwards compatibility break, if someone used the
    generated DBTX for other code that did actually call PrepareContext.

--- examples/booktest/sqlite/db.go
+++ examples/booktest/sqlite/db.go
@@ -11,7 +11,6 @@ import (
 
 type DBTX interface {
 	ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
-	PrepareContext(context.Context, string) (*sql.Stmt, error)
 	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
 	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
 }
