9295833518c02585afcdf6bc25d389c11e8bf9ea
Jille Timmermans <jille@quis.cx>
2023-08-29 00:44:05 +0200

    feat(mysql): Add parser support for IS [NOT] NULL (#2651)
    to return a boolean column

--- internal/endtoend/testdata/comparisons/mysql/query.sql
+++ internal/endtoend/testdata/comparisons/mysql/query.sql
@@ -24,8 +24,8 @@ SELECT count(*) <> 0 FROM bar;
 -- name: Equal :many
 SELECT count(*) = 0 FROM bar;
 
+-- name: IsNull :many
+SELECT id IS NULL FROM bar;
 
-
-
-
-
+-- name: IsNotNull :many
+SELECT id IS NOT NULL FROM bar;
