5ba765b5ab3794386039bc39c812da40853d8517
Kyle Conroy <kyle@sqlc.dev>
2023-09-25 18:53:03 -0700

    cmd/sqlc: Bump version to v1.22.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Riza, Inc.'
 author = 'Riza, Inc.'
 
 # The full version, including alpha/beta/rc tags
-release = '1.21.0'
+release = '1.22.0'
 
 
 # -- General configuration ---------------------------------------------------
