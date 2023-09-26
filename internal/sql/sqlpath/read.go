53cf3701e6ef1b9b055e678ba671883cbceb9f7c
David Shek <ditreyw@gmail.com>
2023-09-26 18:20:53 +0400

    feat: added queries wildcard

--- internal/sql/sqlpath/read.go
+++ internal/sql/sqlpath/read.go
@@ -13,21 +13,27 @@ import (
 // in .sql. Omits hidden files, directories, and migrations.
 func Glob(paths []string) ([]string, error) {
 	var files []string
-	for _, path := range paths {
-		f, err := os.Stat(path)
+	for _, pathForGlob := range paths {
+		globPaths, err := filepath.Glob(pathForGlob)
 		if err != nil {
-			return nil, fmt.Errorf("path %s does not exist", path)
+			return nil, err
 		}
-		if f.IsDir() {
-			listing, err := os.ReadDir(path)
+		for _, path := range globPaths {
+			f, err := os.Stat(path)
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("path %s does not exist", path)
 			}
-			for _, f := range listing {
-				files = append(files, filepath.Join(path, f.Name()))
+			if f.IsDir() {
+				listing, err := os.ReadDir(path)
+				if err != nil {
+					return nil, err
+				}
+				for _, f := range listing {
+					files = append(files, filepath.Join(path, f.Name()))
+				}
+			} else {
+				files = append(files, path)
 			}
-		} else {
-			files = append(files, path)
 		}
 	}
 	var sqlFiles []string
