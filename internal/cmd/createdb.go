2c1120224e6ee41dbdef341ce53308a55061c38d
Kyle Conroy <kyle@sqlc.dev>
2023-10-20 12:28:31 -0700

    Just print to stdout

--- internal/cmd/createdb.go
+++ internal/cmd/createdb.go
@@ -2,16 +2,13 @@ package cmd
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"os"
-	"os/exec"
 	"runtime/trace"
-	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/sqlc-dev/sqlc/internal/config"
-	"github.com/sqlc-dev/sqlc/internal/opts"
+	"github.com/sqlc-dev/sqlc/internal/migrations"
 	"github.com/sqlc-dev/sqlc/internal/quickdb"
 	pb "github.com/sqlc-dev/sqlc/internal/quickdb/v1"
 	"github.com/sqlc-dev/sqlc/internal/sql/sqlpath"
@@ -20,103 +17,74 @@ import (
 var createDBCmd = &cobra.Command{
 	Use:   "createdb",
 	Short: "Create an ephemeral database",
-	Args:  cobra.MinimumNArgs(1),
+	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		defer trace.StartRegion(cmd.Context(), "createdb").End()
 		stderr := cmd.ErrOrStderr()
 		dir, name := getConfigPath(stderr, cmd.Flag("file"))
-		env, err := cmd.Flags().GetString("env")
-		if err != nil {
-			return err
-		}
-		code, err := CreateDB(cmd.Context(), dir, name, args, env, &Options{
+		err := CreateDB(cmd.Context(), dir, name, &Options{
 			Env:    ParseEnv(cmd),
 			Stderr: stderr,
 		})
 		if err != nil {
 			fmt.Fprintln(stderr, err.Error())
-			os.Exit(code)
+			os.Exit(1)
 		}
 		return nil
 	},
 }
 
-func CreateDB(ctx context.Context, dir, filename string, args []string, env string, o *Options) (int, error) {
-	dbg := opts.DebugFromEnv()
-	if !dbg.ProcessPlugins {
-		return 1, fmt.Errorf("process-plugins disabled")
-	}
+func CreateDB(ctx context.Context, dir, filename string, o *Options) error {
 	_, conf, err := o.ReadConfig(dir, filename)
 	if err != nil {
-		return 1, err
+		return err
 	}
-	// Find the first SQL with a managed database
-	var pkg *config.SQL
+	// Find the first queryset with a managed database
+	var queryset *config.SQL
+	var count int
 	for _, sql := range conf.SQL {
 		sql := sql
 		if sql.Database != nil && sql.Database.Managed {
-			pkg = &sql
-			break
+			queryset = &sql
+			count += 1
 		}
 	}
-	if pkg == nil {
-		return 1, fmt.Errorf("no managed database found")
+	if queryset == nil {
+		return fmt.Errorf("no querysets configured to use a managed database")
 	}
-	if pkg.Engine != config.EnginePostgreSQL {
-		return 1, fmt.Errorf("managed: only PostgreSQL currently")
+	if count > 1 {
+		return fmt.Errorf("multiple querysets configured to use managed databases")
+	}
+	if queryset.Engine != config.EnginePostgreSQL {
+		return fmt.Errorf("managed databases currently only support PostgreSQL")
 	}
 
-	var migrations []string
-	files, err := sqlpath.Glob(pkg.Schema)
+	var ddl []string
+	files, err := sqlpath.Glob(queryset.Schema)
 	if err != nil {
-		return 1, err
+		return err
 	}
 	for _, schema := range files {
 		contents, err := os.ReadFile(schema)
 		if err != nil {
-			return 1, fmt.Errorf("read file: %w", err)
+			return fmt.Errorf("read file: %w", err)
 		}
-		migrations = append(migrations, string(contents))
+		ddl = append(ddl, migrations.RemoveRollbackStatements(string(contents)))
 	}
+
 	client, err := quickdb.NewClientFromConfig(conf.Cloud)
 	if err != nil {
-		return 1, fmt.Errorf("client error: %w", err)
+		return fmt.Errorf("client error: %w", err)
 	}
 
 	resp, err := client.CreateEphemeralDatabase(ctx, &pb.CreateEphemeralDatabaseRequest{
 		Engine:     "postgresql",
 		Region:     quickdb.GetClosestRegion(),
-		Migrations: migrations,
+		Migrations: ddl,
 	})
 	if err != nil {
-		return 1, fmt.Errorf("managed: create database: %w", err)
+		return fmt.Errorf("managed: create database: %w", err)
 	}
-
-	defer func() {
-		client.DropEphemeralDatabase(ctx, &pb.DropEphemeralDatabaseRequest{
-			DatabaseId: resp.DatabaseId,
-		})
-	}()
-
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", env, resp.Uri))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Env = []string{fmt.Sprintf("%s=%s", env, resp.Uri)}
-	for _, val := range os.Environ() {
-		if strings.HasPrefix(val, "SQLC_AUTH_TOKEN") {
-			continue
-		}
-		cmd.Env = append(cmd.Env, val)
-	}
-
-	if err := cmd.Run(); err != nil {
-		var exitErr *exec.ExitError
-		if errors.As(err, &exitErr) {
-			return exitErr.ExitCode(), err
-		}
-		return 1, err
-	}
-
-	return 0, nil
+	fmt.Println(resp.Uri)
+	return nil
 }
