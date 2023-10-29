da350ebbd6fca9f95b7dc3cc82246cd5ad2e101e
Nao Yonashiro <owan.orisano@gmail.com>
2023-10-30 03:28:17 +0900

    fix(engine/sqlite): support CASE expr
    close #2925

---
+++ internal/endtoend/testdata/case_named_params/sqlite/query.sql
@@ -0,0 +1,6 @@
+-- name: ListAuthors :one
+SELECT  *
+FROM    authors
+WHERE   email = CASE WHEN sqlc.arg(email) = '' then NULL else sqlc.arg(email) END
+        OR username = CASE WHEN sqlc.arg(username) = '' then NULL else sqlc.arg(username) END
+LIMIT   1;
---
+++ internal/endtoend/testdata/case_named_params/sqlite/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE authors (
+  id   INTEGER PRIMARY KEY,
+  username TEXT NULL,
+  email TEXT NULL,
+  name TEXT  NOT NULL,
+  bio  TEXT
+);
+
