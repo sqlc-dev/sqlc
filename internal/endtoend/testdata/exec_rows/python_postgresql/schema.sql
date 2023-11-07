a334908d8230aeb39a10fc350634e9d83c0c1403
Kyle Gray <kyle@sqlc.dev>
2023-11-06 17:32:41 -0800

    test: New workflow for sqlc-gen-python (#2936)
    * test: New workflow for sqlc-gen-python
    
    * fix workflow
    
    * Run diff in the python examples
    
    * docs: Remove Python examples
    
    They now live in sqlc-gen-python
    
    * test: Remove all python tests
    
    They've been ported to the Python repository

--- internal/endtoend/testdata/exec_rows/python_postgresql/schema.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE bar (id serial not null);
-
--- internal/endtoend/testdata/query_parameter_limit/-1/python_postgresql/query.sql
+++
@@ -1,5 +0,0 @@
--- name: DeleteBarByID :execrows
-DELETE FROM bar WHERE id = $1;
-
--- name: DeleteBarByIDAndName :execrows
-DELETE FROM bar WHERE id = $1 AND name = $2;
