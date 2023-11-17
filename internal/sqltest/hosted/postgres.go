a113b6780abf363a7bf3e2a9b8a76df89ac78467
Kyle Gray <kyle@sqlc.dev>
2023-11-17 12:49:47 -0800

    feat(examples): Use hosted MySQL databases for tests (#2982)
    * feat(examples): Use hosted MySQL databases for tests
    
    * Switch to managed mode for more examples

--- internal/sqltest/hosted/postgres.go
+++ internal/sqltest/hosted/postgres.go
@@ -2,32 +2,13 @@ package hosted
 
 import (
 	"context"
-	"fmt"
 	"os"
-	"sync"
 	"testing"
 
 	"github.com/sqlc-dev/sqlc/internal/quickdb"
 	pb "github.com/sqlc-dev/sqlc/internal/quickdb/v1"
 	"github.com/sqlc-dev/sqlc/internal/sql/sqlpath"
 )
-
-var client pb.QuickClient
-var once sync.Once
-
-func initClient() error {
-	projectID := os.Getenv("CI_SQLC_PROJECT_ID")
-	authToken := os.Getenv("CI_SQLC_AUTH_TOKEN")
-	if projectID == "" || authToken == "" {
-		return fmt.Errorf("missing project id or auth token")
-	}
-	c, err := quickdb.NewClient(projectID, authToken)
-	if err != nil {
-		return err
-	}
-	client = c
-	return nil
-}
 
 func PostgreSQL(t *testing.T, migrations []string) string {
 	ctx := context.Background()
