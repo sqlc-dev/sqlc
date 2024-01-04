2b731202e22c5990fb54cec15355dc386846bdeb
Kyle Gray <kyle@sqlc.dev>
2024-01-03 16:07:31 -0800

    feat(sqlc): Bump to version v1.25.0 (#3102)
    * feat(sqlc): Bump to version v1.25.0
    
    * test: Update testdata for version v1.25.0
    
    * docs: Update examples for version v1.25.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Riza, Inc.'
 author = 'Riza, Inc.'
 
 # The full version, including alpha/beta/rc tags
-release = '1.24.0'
+release = '1.25.0'
 
 
 # -- General configuration ---------------------------------------------------
--- internal/info/facts.go
+++ internal/info/facts.go
@@ -2,4 +2,4 @@ package info
 
 // When no version is set, return the next bug fix version
 // after the most recent tag
-const Version = "v1.24.0"
+const Version = "v1.25.0"
