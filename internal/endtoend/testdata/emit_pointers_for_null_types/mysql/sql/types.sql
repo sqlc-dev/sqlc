7331e37a044867d1561c74052777b7caaecb25a1
yoRyuuuuu <sasoseooo@gmail.com>
2025-03-12 00:01:06 +0900

    feat: add end-to-end tests for emitting pointers for nullable types in MySQL code generation

---
+++ internal/endtoend/testdata/emit_pointers_for_null_types/mysql/sql/types.sql
@@ -0,0 +1,17 @@
+CREATE TABLE dt_integer (
+    a tinyint,
+    b smallint,
+    c int,
+    d integer,
+    e mediumint,
+    f bigint
+);
+
+CREATE TABLE dt_unsigned_integer (
+    a tinyint unsigned,
+    b smallint unsigned,
+    c int unsigned,
+    d integer unsigned,
+    e mediumint unsigned,
+    f bigint unsigned
+);
