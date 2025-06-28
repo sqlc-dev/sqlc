dd314ca83b01862818626dcbf261a58c50682efa
or orsatti <antonio@10play.dev>
2025-06-28 21:30:28 +0300

    test: rtee test cases

--- internal/endtoend/testdata/virtual_table/sqlite/schema.sql
+++ internal/endtoend/testdata/virtual_table/sqlite/schema.sql
@@ -13,3 +13,38 @@ INSERT INTO tbl VALUES(2, 'aa bb', 't', 'a', 22);
 
 INSERT INTO ft VALUES('xx cc');
 INSERT INTO ft VALUES('cc bb');
+
+CREATE TABLE weather (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL
+);
+
+
+CREATE VIRTUAL TABLE weather_rtree USING rtree(
+  id,
+  min_lang, max_long,
+  min_lat, max_lat
+);
+
+CREATE TRIGGER weather_insert
+AFTER INSERT
+  ON weather BEGIN
+INSERT INTO
+  weather_rtree (
+    id,
+    min_lang,
+    max_long,
+    min_lat,
+    max_lat
+  )
+VALUES
+  (
+    NEW.id,
+    NEW.latitude,
+    NEW.latitude,
+    NEW.longitude,
+    NEW.longitude
+  );
+
+END;
