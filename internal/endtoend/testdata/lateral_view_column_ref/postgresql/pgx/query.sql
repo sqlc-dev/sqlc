2d7b302a85a2da586681dadd514f836360be4ec5
Will Roden <will@roden.cc>
2025-11-26 14:46:51 -0600

    LATERAL subquery support

---
+++ internal/endtoend/testdata/lateral_view_column_ref/postgresql/pgx/query.sql
@@ -0,0 +1,9 @@
+-- name: GetFooLateral :many
+SELECT val, result FROM foo_lateral;
+
+-- name: GetFooLateralDirect :many
+SELECT f.id, f.val, sub.result
+FROM foo f
+CROSS JOIN LATERAL (
+  SELECT f.val || '-direct' AS result
+) sub;
