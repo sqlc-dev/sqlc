b1d6767856fd18befc2bff6747daf37263752f05
Will Roden <will@roden.cc>
2025-11-28 11:19:33 -0600

    move test

--- internal/endtoend/testdata/lateral_view_column_ref/postgresql/pgx/v5/query.sql
+++
@@ -1,9 +0,0 @@
--- name: GetFooLateral :many
-SELECT val, result FROM foo_lateral;
-
--- name: GetFooLateralDirect :many
-SELECT f.id, f.val, sub.result
-FROM foo f
-CROSS JOIN LATERAL (
-  SELECT (f.val || '-direct')::text AS result
-) sub;
