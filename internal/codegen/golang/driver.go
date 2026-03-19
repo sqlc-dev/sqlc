ece0a7e489c82694518985ebbd645dc12c3bac3a
Suman De <sumande@Sumans-MacBook-Air.local>
2026-03-19 18:15:10 +0530

    refactor

--- internal/codegen/golang/driver.go
+++ internal/codegen/golang/driver.go
@@ -19,9 +19,7 @@ func parseDriverPGType(sqlPackage string) opts.SQLDriver {
 	switch sqlPackage {
 	case opts.SQLPackagePGXV4:
 		return opts.SQLDriverPGXV4
-	case opts.SQLPackagePGXV5:
-		return opts.SQLDriverPGXV5
-	case opts.SQLPackageYugaBytePGXV5:
+	case opts.SQLPackagePGXV5, opts.SQLPackageYugaBytePGXV5:
 		return opts.SQLDriverPGXV5
 	default:
 		return opts.SQLDriverLibPQ
