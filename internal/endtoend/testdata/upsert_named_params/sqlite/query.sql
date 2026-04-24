4c7db803d585a395742e86daa2ad6c47731c0c07
Chinmay S B <cbh.mavim@gmail.com>
2026-04-24 21:16:42 +0530

    test: add WHERE clause variations to upsert_named_params/sqlite

--- internal/endtoend/testdata/upsert_named_params/sqlite/query.sql
+++ internal/endtoend/testdata/upsert_named_params/sqlite/query.sql
@@ -11,3 +11,49 @@ INSERT INTO authors (
   name = @name,
   bio = sqlc.narg('bio')
 RETURNING *;
+
+-- name: UpsertAuthorDoUpdateWhere :one
+INSERT INTO authors (
+  id,
+  name,
+  bio
+) VALUES (
+  @id,
+  @name,
+  sqlc.narg('bio')
+) ON CONFLICT(id) DO UPDATE SET
+  name = @name,
+  bio = sqlc.narg('bio')
+WHERE excluded.name != @name
+RETURNING *;
+
+-- name: UpsertAuthorConflictWhere :one
+INSERT INTO authors (
+  id,
+  name,
+  bio
+) VALUES (
+  @id,
+  @name,
+  sqlc.narg('bio')
+) ON CONFLICT(id) DO UPDATE SET
+  name = @name,
+  bio = sqlc.narg('bio')
+WHERE excluded.bio != sqlc.narg('bio')
+RETURNING *;
+
+-- name: UpsertAuthorBothWhere :one
+INSERT INTO authors (
+  id,
+  name,
+  bio
+) VALUES (
+  @id,
+  @name,
+  sqlc.narg('bio')
+) ON CONFLICT(id) DO UPDATE SET
+  name = @name,
+  bio = sqlc.narg('bio')
+WHERE excluded.name != @name
+AND excluded.bio != sqlc.narg('bio')
+RETURNING *;
